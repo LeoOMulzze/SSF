32f8a05293d6b34640126cd95493ad1a2239dfa4
Leonardo Cruz <163577394+LeoOMulzze@users.noreply.github.com>
2024-04-29 19:16:55 -0300

    Update Casa.java

--- src/Modelo/Casa.java
+++ src/Modelo/Casa.java
@@ -1,29 +1,41 @@
 package Modelo;
 
+import Util.DescontoMaiorDoQueJurosException;
+
 public class Casa extends Financiamento {
     // Atributos para armazenar a área construída e a área do terreno da casa.
     protected double areaConstruida;
     protected double areaTerreno;
+    protected double desconto;
 
-    public double getAreaConstruida() {
+    public double getAreaConstruida() { // getter para a área Construida
         return areaConstruida;
     }
 
-    public double getAreaTerreno() {
+    public double getAreaTerreno() { // getter para a área de terreno
         return areaTerreno;
     }
 
-    // Construtor da classe Casa.
-    public Casa(double valor, int prazoPagamento, double taxaJurosAnual, double areaConstruida, double areaTerreno) {
-        // Chamada ao construtor da superclasse Financiamento
-        super(valor, prazoPagamento, taxaJurosAnual);
-        this.areaConstruida = areaConstruida;
-        this.areaTerreno = areaTerreno;
+    public double getDesconto() { // getter para o desconto
+        return desconto;
     }
 
-    // Método para calcular o pagamento mensal, com um acréscimo de R$80.
-    @Override
+    // Construtor da classe Casa.
+    public Casa(double valor, int prazoPagamento, double taxaJurosAnual, double areaConstruida, double areaTerreno, double desconto) throws DescontoMaiorDoQueJurosException {
+        // Chamada ao construtor da superclasse Financiamento
+        super(valor, prazoPagamento, taxaJurosAnual);
+
+        // Verifica se o desconto é maior que o juro mensal
+        if (desconto > super.setCalcularPagamentoMensal()) {
+            throw new DescontoMaiorDoQueJurosException();
+        }
+
+        this.areaConstruida = areaConstruida;
+        this.areaTerreno = areaTerreno;
+        this.desconto = desconto;
+    }
+
     public double setCalcularPagamentoMensal() {
-        return super.setCalcularPagamentoMensal() + 80;
+        return super.setCalcularPagamentoMensal() - desconto;
     }
 }
